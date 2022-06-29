bb2c721cb339bc5e75f7357cc0d09cf7a593f7f6
FRCat.JS <89495053+French-Cat@users.noreply.github.com>
2022-06-28 23:21:29 -0400

    Update serviceworker.js

--- serviceworker.js
+++ serviceworker.js
@@ -1,4 +1,9 @@
-self.addEventListener("install", event => {
+self.addEventListener("install", (event) => {
+    console.log("install");
 });
-self.addEventListener("activate", event => {
+self.addEventListener("activate", (event) => {
+    console.log("activate");
+});
+self.addEventListener("fetch", (event) => {
+    console.log("fetch");
 });
